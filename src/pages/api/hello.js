9b2814d215d85c908de6f4241edb1c8fde922d7b
ROWEND36 <rowendduke36@gmail.com>
2023-05-01 02:57:50 +0100

    feat: deploy

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+export const config = {
+  runtime: "edge",
+};
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default function handler(req) {
+  return new Response(
+    JSON.stringify({
+      name: "Jim Halpert",
+    }),
+    {
+      status: 200,
+      headers: {
+        "content-type": "application/json",
+      },
+    }
+  );
 }
