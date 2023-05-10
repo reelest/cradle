c03cf5f0920326ca0cc5059643f332c58937176e
ROWEND36 <rowendduke36@gmail.com>
2023-05-10 10:52:09 +0100

    feat: login page

--- src/logic/api.js
+++ src/logic/api.js
@@ -1,12 +1,29 @@
-import useDummyData, { pick } from "@/utils/dummy_data";
-import usePromise from "@/utils/usePromise";
-import { useRef } from "react";
+import { useAsyncDummyData } from "@/utils/dummy_data";
+import { URLSearchParams } from "next/dist/compiled/@edge-runtime/primitives/url";
+import { useRouter } from "next/router";
+import { useMemo } from "react";
+import { useAsync } from "react-use";
 
-//Every API either returns an object or undefined ie loading
-const useAsyncDummyData = (API) => {
-  API = useRef(API).current;
-  const data = useDummyData(API);
-  return usePromise(() => Promise.resolve(data), [data]);
-};
 export const useAnnouncementsAPI = () =>
   useAsyncDummyData({ announcements: ["admission", 1, 1] });
+
+export const forgotPasswordAPIUrl = "/api/resetpassword";
+export const doLogin = () => {
+  localStorage.setItem("logged-in");
+};
+export const useLoginError = () => {
+  const query = useRouter().query;
+  return useMemo(
+    function () {
+      if (query.hasOwnProperty("error-user")) return "Email is not registered";
+      if (query.hasOwnProperty("error-password")) return "Wrong password";
+    },
+    [query]
+  );
+};
+
+export const useLoggedIn = () => {
+  return (
+    useAsync(async () => (await fetch("/api/checklogin")).json()).value == true
+  );
+};
